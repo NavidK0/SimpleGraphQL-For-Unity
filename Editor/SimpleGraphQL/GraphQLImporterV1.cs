da265b64a3115896605b028245461e0b16d73ecd
dazzle-jukka <jukka.linna@dazzle.rocks>
2020-09-07 09:56:09 +0300

    Added typespec handling to graphql importer (#2)
    * Added typespec handling to graphql importer
    
    * Added some comments about replacement operations

--- Editor/SimpleGraphQL/GraphQLImporterV1.cs
+++ Editor/SimpleGraphQL/GraphQLImporterV1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using SimpleGraphQL.GraphQLParser;
 using SimpleGraphQL.GraphQLParser.AST;
 using UnityEditor.Experimental.AssetImporters;
@@ -14,32 +15,47 @@ namespace SimpleGraphQL
     {
         public override void OnImportAsset(AssetImportContext ctx)
         {
-            var lexer = new Lexer();
-            var parser = new Parser(lexer);
+            Lexer lexer = new Lexer();
+            Parser parser = new Parser(lexer);
+            // Match against all lines starting with "#import "
+            // and assign values coming after it to a group called path
+            Regex regex = new Regex("#import \\\"(?<path>[\\w\\/.]+)\\\"");
             string contents = File.ReadAllText(ctx.assetPath);
+            foreach(Match match in regex.Matches(contents))
+            {
+                string filePath = Path.Combine(Path.GetDirectoryName(ctx.assetPath), match.Groups["path"].Value);
+                // Since the filePath contains . and .. to traverse current / parent directory
+                // we need to convert the relative path to an actual path
+                // and then read contents of that file
+                string replacementContents = File.ReadAllText(Path.GetFullPath(filePath));
+
+                // Replace the #import directive as a whole with the replacement contents
+                contents = contents.Replace(match.Value, replacementContents);
+            }
+
             string fileName = Path.GetFileNameWithoutExtension(ctx.assetPath);
-            var queryFile = ScriptableObject.CreateInstance<GraphQLFile>();
+            GraphQLFile queryFile = ScriptableObject.CreateInstance<GraphQLFile>();
 
             GraphQLDocument graphQLDocument = parser.Parse(new Source(contents));
 
             List<GraphQLOperationDefinition> operations = graphQLDocument.Definitions
                 .FindAll(x => x.Kind == ASTNodeKind.OperationDefinition)
-                .Select(x => (GraphQLOperationDefinition) x)
+                .Select(x => (GraphQLOperationDefinition)x)
                 .ToList();
 
-            if (operations.Count > 0)
+            if(operations.Count > 0)
             {
-                foreach (GraphQLOperationDefinition operation in operations)
+                foreach(GraphQLOperationDefinition operation in operations)
                 {
                     // Check for multiple anonymous queries (not allowed by graphQL)
                     // Also checks for anonymous queries inside a file with named queries
-                    if (queryFile.Queries.Count > 1 && operation.Name == null)
+                    if(queryFile.Queries.Count > 1 && operation.Name == null)
                     {
                         throw new ArgumentException(
                             $"Multiple anonymous queries/anonymous query with named query found within: {ctx.assetPath}\nPlease ensure that there is either only one anonymous query, or all queries are named within the file!");
                     }
 
-                    if (!Enum.TryParse(operation.Operation.ToString(), out OperationType operationType))
+                    if(!Enum.TryParse(operation.Operation.ToString(), out OperationType operationType))
                     {
                         Debug.LogWarning("Unable to convert operation type in " + ctx.assetPath);
                     }
@@ -53,7 +69,8 @@ namespace SimpleGraphQL
                     });
                 }
             }
-            else
+            // #typespec is a custom definition indicating that the file might not contain queries
+            else if(string.IsNullOrEmpty(contents) || !contents.StartsWith("#typespec"))
             {
                 throw new ArgumentException(
                     $"There were no operation definitions inside this graphql: {ctx.assetPath}\nPlease ensure that there is at least one operation defined!");
