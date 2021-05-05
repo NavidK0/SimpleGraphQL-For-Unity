fea43510c8188eb3bfe441eb2eb1df39fa221ab2
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2021-05-05 14:32:32 +0200

    Add deserialization of error responses

--- Runtime/SimpleGraphQL/Response.cs
+++ Runtime/SimpleGraphQL/Response.cs
@@ -8,5 +8,34 @@ namespace SimpleGraphQL
     {
         [DataMember(Name = "data")]
         public T Data { get; set; }
+
+        [DataMember(Name = "errors")]
+        [CanBeNull]
+        public Error[] Errors { get; set; }
+    }
+
+    [PublicAPI]
+    public class Error
+    {
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+
+        [DataMember(Name = "locations")]
+        [CanBeNull]
+        public Location[] Locations { get; set; }
+
+        [DataMember(Name = "path")]
+        [CanBeNull]
+        public object[] Path { get; set; } // Path objects can be either integers or strings
+    }
+
+    [PublicAPI]
+    public class Location
+    {
+        [DataMember(Name = "line")]
+        public int Line { get; set; }
+
+        [DataMember(Name = "column")]
+        public int Column { get; set; }
     }
 }
